b626fa2e1caa52016f15769280e073d853a04891
M-S-KHAN <sajawalkhan111@gmail.com>
2024-06-07 19:51:56 +0100

    - UI for Login ready including footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-    <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+      <body className={inter.className}>{children}
+
+      </body>
+
+      </html>
   );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,28 +4,39 @@ import img from "../../public/images/logo-main-circle.png";
 
 export default function Login() {
     return (
-        <div className="flex min-h-screen bg-base-200 justify-center items-center">
+        <div className="flex flex-col min-h-screen">
             <Head>
                 <title>Login</title>
             </Head>
-            <div className="card w-96 bg-base-100 shadow-xl m-10">
-                <div className="card-body flex justify-center items-center">
-                    <Image src={img} alt="Logo Image" width={150} height={150} className="m-2 "></Image>
-                    <form className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" placeholder="Email" className="input input-bordered input-primary w-full" />
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" placeholder="Password" className="input input-bordered input-primary w-full" />
-                        <div className="form-control mt-6">
-                            <button type="submit" className="btn btn-primary">Login</button>
-                        </div>
-                    </form>
+            <div className="flex-grow flex bg-base-200 justify-center items-center">
+                <div className="card w-96 bg-base-100 shadow-xl m-10 p-7">
+                    <div className="card-body flex justify-center items-center">
+                        <Image src={img} alt="Logo Image" width={150} height={150} className="m-2 "></Image>
+                        <form className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="Email"
+                                   className="input input-bordered input-primary w-full"/>
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="Password"
+                                   className="input input-bordered input-primary w-full"/>
+                            <div className="form-control mt-6">
+                                <button type="submit" className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+
                 </div>
+
             </div>
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>Copyright © 2024 - All right reserved by Workr.</p>
+                </aside>
+            </footer>
         </div>
     );
 }
