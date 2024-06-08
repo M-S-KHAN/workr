50371468be6d86a5a201f26c54db6a81de2ee5c8
M-S-KHAN <sajawalkhan111@gmail.com>
2024-06-08 20:50:41 +0100

    - Suspense updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {Suspense} from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,10 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+      <Suspense>
 
+          {children}</Suspense>
       </body>
 
       </html>
